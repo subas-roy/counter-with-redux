a450c1c99726133f46ad99184e848d7e7711fd52
subas-roy <subasroy46@gmail.com>
2025-02-12 01:25:08 +0600

    Create counterSlice

--- __steps__.js
+++ __steps__.js
@@ -2,5 +2,7 @@
  *
  * 1. npm create vite@latest counter-with-redux -- --template react-ts
  * 2. npm install @reduxjs/toolkit react-redux
+ * 3. Check store is connected with app (redux dev tools) "@@INIT"
+ * 4. Create counter slice
  *
  */
