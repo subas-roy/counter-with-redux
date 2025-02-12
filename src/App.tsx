27836a76fb3f479a4432448e2d52d108a712f0c0
subas-roy <subasroy46@gmail.com>
2025-02-12 12:52:16 +0600

    Get state in component level using useSelector()

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { decrement, increment } from './redux/features/counter/counterSlice';
 
 function App() {
-  // hook
-  const dispatch = useDispatch();
+  const dispatch = useDispatch(); // hook
+  const { count } = useSelector((state) => state.counter);
 
   const handleIncrement = () => {
     dispatch(increment());
@@ -17,7 +17,7 @@ function App() {
     <div>
       <h1>Counter with Redux</h1>
       <button onClick={handleIncrement}>Increment</button>
-      <div>0</div>
+      <div>{count}</div>
       <button onClick={handleDecrement}>Decrement</button>
     </div>
   );
